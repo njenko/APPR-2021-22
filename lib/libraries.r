151fe1803e2ee796024041f7609cb97be333ea51
njenko <nejc.jenko1@gmail.com>
2021-12-01 19:31:30 +0100

    clone

---
+++ lib/libraries.r
@@ -0,0 +1,14 @@
+library(knitr)
+library(rvest)
+library(gsubfn)
+library(tidyr)
+library(tmap)
+library(shiny)
+library(readr)
+library(dplyr)
+library(tibble)
+
+options(gsubfn.engine="R")
+
+# Uvozimo funkcije za pobiranje in uvoz zemljevida.
+source("lib/uvozi.zemljevid.r", encoding="UTF-8")
