c9fc60635700a6a2f6c68a13786f0f7e75f33ae9
njenko <nejc.jenko1@gmail.com>
2022-08-20 20:07:16 +0200

    Random stuff

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -1,4 +1,11 @@
 # 2. faza: Uvoz podatkov
 
+library(rvest)
+library(XML)
 
-sl <- locale("sl", decimal_mark=",", grouping_mark=".")
+url <- 'https://www.premierleague.com/players'
+url2 <- "file:///Users/nejcjenko/Downloads/understat_player.csv.html"
+
+players_EPL_22_23 <- read_html(url) %>% html_table(fill=TRUE) %>% .[[1]]
+
+players_EPL_22_23_stats <- read_html(url2) %>% html_table(fill=TRUE) %>% .[[1]]
