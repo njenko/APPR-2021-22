9b557f5ec79e63518d2a8e838bdaa59e6f62e7c7
njenko <nejc.jenko1@gmail.com>
2022-08-22 08:05:45 +0200

    trying stuff

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -6,18 +6,17 @@ library(XML)
 # Tabla s seznamom igralcev in njihovimi državami
 url <- 'https://www.premierleague.com/players'
 
-#Start the web driver 
 driver <- rsDriver(browser=c("firefox"), port = 4445)
-#Say 'I'm a client'    
 remote_driver <- driver[["client"]]
-#Send to browser the web site address    
 remote_driver$navigate(url)
-#Find in the css enviroment the body   
 scroll_d <- remote_driver$findElement(using = "css", value = "body")
-#send to the browser to go to the end of the page
 scroll_d$sendKeysToElement(list(key = "end"))
 
-players_EPL <- read_html(remote_driver) %>% html_table(fill=TRUE) %>% .[[1]]
+#parse it
+players_EPL <- read_html(scroll_d) %>% html_table(fill = TRUE) %>%
+  .[[1]]
+
+#players_EPL <- read_html(url) %>% html_table(fill=TRUE) %>% .[[1]]
 
 
 # Tabela s statistikami
