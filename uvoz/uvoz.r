73f57b3f787fcb24ec43a792a79f34259d21a2e5
njenko <nejc.jenko1@gmail.com>
2022-08-22 08:48:12 +0200

    players csv add

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -3,20 +3,24 @@ library(RSelenium)
 library(rvest)
 library(XML)
 
-# Tabla s seznamom igralcev in njihovimi državami
-url <- 'https://www.premierleague.com/players'
 
-driver <- rsDriver(browser=c("firefox"), port = 4445L)
-remote_driver <- driver[["client"]]
-remote_driver$navigate(url)
-scroll_d <- remote_driver$findElement(using = "css", value = "body")
-scroll_d$sendKeysToElement(list(key = "end"))
-
-players_EPL <- read_html(scroll_d) %>% html_table(fill = TRUE) %>%
-  .[[1]]
-
-#players_EPL <- read_html(url) %>% html_table(fill=TRUE) %>% .[[1]]
+# # Tabla s seznamom igralcev in njihovimi državami
+# url <- 'https://www.premierleague.com/players'
+# 
+# driver <- rsDriver(browser=c("firefox"), port = 4445L)
+# remote_driver <- driver[["client"]]
+# remote_driver$navigate(url)
+# scroll_d <- remote_driver$findElement(using = "css", value = "body")
+# scroll_d$sendKeysToElement(list(key = "end"))
+# 
+# players_EPL <- read_html(scroll_d) %>% html_table(fill = TRUE) %>%
+#   .[[1]]
+# 
+# #players_EPL <- read_html(url) %>% html_table(fill=TRUE) %>% .[[1]]
 
 
 # Tabela s statistikami
 player_stats <- read.csv("Podatki/understat_player.csv", header = TRUE, skip = 2)
+
+# Tabela z igralci in nacionalnostmi
+players_EPL <- read.csv("Podatki/PL_Players.csv", header = TRUE, skip = 2)
