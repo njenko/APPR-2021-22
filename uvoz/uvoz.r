3a09ef6ee3a0e51efd9037ce5c3d9ef1c5b7d593
njenko <nejc.jenko1@gmail.com>
2022-08-22 07:33:08 +0200

    uvoz edits

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -1,11 +1,24 @@
 # 2. faza: Uvoz podatkov
-
+library(RSelenium)
 library(rvest)
 library(XML)
 
+# Tabla s seznamom igralcev in njihovimi državami
 url <- 'https://www.premierleague.com/players'
-url2 <- "file:///Users/nejcjenko/Downloads/understat_player.csv.html"
 
-players_EPL_22_23 <- read_html(url) %>% html_table(fill=TRUE) %>% .[[1]]
+#Start the web driver 
+driver <- rsDriver(browser=c("firefox"), port = 4445)
+#Say 'I'm a client'    
+remote_driver <- driver[["client"]]
+#Send to browser the web site address    
+remote_driver$navigate(url)
+#Find in the css enviroment the body   
+scroll_d <- remote_driver$findElement(using = "css", value = "body")
+#send to the browser to go to the end of the page
+scroll_d$sendKeysToElement(list(key = "end"))
 
-players_EPL_22_23_stats <- read_html(url2) %>% html_table(fill=TRUE) %>% .[[1]]
+players_EPL <- read_html(remote_driver) %>% html_table(fill=TRUE) %>% .[[1]]
+
+
+# Tabela s statistikami
+player_stats <- read.csv("Podatki/understat_player.csv", header = TRUE, skip = 2)
