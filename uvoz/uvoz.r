aa0621ea17c6bbd0ed1b62051c874cc12169d594
njenko <nejc.jenko1@gmail.com>
2022-08-24 18:18:00 +0200

    fml

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -4,7 +4,7 @@ library(rvest)
 library(XML)
 library(dplyr)
 
-
+#=============================================================
 
 # # Tabla s seznamom igralcev in njihovimi državami
 # url <- 'https://www.premierleague.com/players'
@@ -20,21 +20,30 @@ library(dplyr)
 # 
 # #players_EPL <- read_html(url) %>% html_table(fill=TRUE) %>% .[[1]]
 
-
+#=============================================================
+#=============================================================
 
 # Tabela s statistikami - Gameweek 2
-player_stats_GW2 <- read.csv("Podatki/player_stats_GW2.csv", header = TRUE, skip = 2) 
+player_stats_GW2 <- read.csv("Podatki/player_stats_GW2.csv", header = TRUE, skip = 2, , fileEncoding = "utf8") 
 player_stats_GW2 <- player_stats_GW2[order(player_stats_GW2$player_name), ]
 
+#=============================================================
+#=============================================================
+
 #Tabela s statistikami - Gameweek 3
-player_stats_GW3 <- read.csv("Podatki/player_stats_GW3.csv", header = TRUE, skip = 2) 
+player_stats_GW3 <- read.csv("Podatki/player_stats_GW3.csv", header = TRUE, skip = 2, fileEncoding = "utf8") 
 player_stats_GW3 <- player_stats_GW3[order(player_stats_GW3$player_name), ]
 
+#=============================================================
+#=============================================================
+
 # Tabela z igralci in nacionalnostmi
-players_EPL <- read.csv("Podatki/PL_Players.csv", header = TRUE, skip = 2) 
+players_EPL <- read.csv("Podatki/PL_Players.csv", header = TRUE, skip = 2, fileEncoding = "utf8") 
 players_EPL <- players_EPL[-c(4, 5)]
 players_EPL <- players_EPL[order(players_EPL$Player), ]
 
+#=============================================================
+#=============================================================
 
 # Združimo v tabelo statistiko GW3 in podatke o pozicijah ter nacionalnostih
 # Left-join, saj je v tabeli z igralci veliko vec igralcev, ki še niso/ne bodo igrali
@@ -42,6 +51,8 @@ players_nat_goals <- merge(players_EPL, player_stats_GW3, by.x = 1, by.y = 2, al
   select(-21) %>% 
   drop_na()
 
+#=============================================================
+#=============================================================
 
 #Get the table with team stats from the website 
 url <- "https://fbref.com/en/comps/9/Premier-League-Stats"
@@ -53,4 +64,31 @@ header <- as.character(as.vector(team_stats[1,]))
 names(team_stats) <- header
 team_stats <- team_stats[-1, ]
 
-players_nat_goals %>% write.csv("podatki/Tabela2.csv", fileEncoding = "utf8")
+
+#=============================================================
+# Write the tables I intend to use
+#=============================================================
+
+tabela1 <- players_nat_goals %>% select("Player", "Nationality", "team_title", "Position", "goals", "assists")
+tabela1 %>% write.csv("podatki/Tabela1.csv", fileEncoding = "utf8")
+
+#=============================================================
+
+tabela2_1 <- player_stats_GW2 %>% select("player_name", "goals", "assists", "xG", "xA")
+colnames(tabela2_1)[2] <- "goals_GW2"
+colnames(tabela2_1)[3] <- "assists_GW2"
+colnames(tabela2_1)[4] <- "xG_GW2"
+colnames(tabela2_1)[5] <- "xA_GW2"
+
+tabela2_2 <- player_stats_GW3 %>% select("player_name", "goals", "assists", "xG", "xA")
+colnames(tabela2_2)[2] <- "goals_GW3"
+colnames(tabela2_2)[3] <- "assists_GW3"
+colnames(tabela2_2)[4] <- "xG_GW3"
+colnames(tabela2_2)[5] <- "xA_GW3"
+
+tabela2 <- merge(tabela2_1, tabela2_2, all.x = T)
+tabela2 %>% write.csv("podatki/Tabela2.csv", fileEncoding = "utf8")
+
+#=============================================================
+
+
--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -1,2 +1,34 @@
 # 3. faza: Vizualizacija podatkov
 
+library(dplyr)
+#=============================================================
+# Podatki
+#=============================================================
+
+podatki1 <- read.csv("podatki/Tabela1.csv", fileEncoding = "utf8")
+podatki2 <- read.csv("podatki/Tabela2.csv", fileEncoding = "utf8")
+
+#=============================================================
+
+# Tablea, s številom golov po državah
+
+tabela1 <- podatki1 %>%
+  select(c("Nationality", "goals")) %>% 
+  group_by(Nationality) %>%
+  summarise(Goals = sum(goals)) %>%
+  subset(Goals != 0)
+
+
+#graf1 = tabela1 %>%
+
+ggplot(tabela1, aes(x = Nationality, y = Goals)) +
+  geom_bar(stat =  "identity", fill="steelblue")+
+  geom_text(aes(label=Goals))+
+  theme_minimal()
+
+
+
+
+
+
+
